be6e9520199e7014063872d92454abfffcd67d02
Esan Natraj <esannatraj8@gmail.com>
2025-01-18 20:47:32 +0800

    fixing formatting

--- src/hash_table.c
+++ src/hash_table.c
@@ -3,11 +3,20 @@
 
 #include "hash_table.h"
 
-static ht_item *ht_new_item(char *k, char *v)
-{
-    ht_item *i = malloc(sizeof(ht_item));
+static ht_item* ht_new_item(char* k, char* v) {
+    ht_item* i = malloc(sizeof(ht_item));
     i->key = strdup(k);
     i->value = strdup(v);
 
     return i;
 }
+
+ht_hash_table* ht_new() {
+    ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+    ht->size = 53;
+    ht->count = 0;
+    ht->items = calloc((size_t)ht->size, sizeof(ht_item*));
+
+    return ht;
+}
+
